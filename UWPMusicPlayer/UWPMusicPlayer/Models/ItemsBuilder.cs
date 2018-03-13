26e31926927a21eecf2fb31cfe610579047315f0
w326004741 <326004741@qq.com>
2018-03-13 14:29:21 +0000

    update ItemsBuilder.cs(change navitem pages)

--- UWPMusicPlayer/UWPMusicPlayer/Models/ItemsBuilder.cs
+++ UWPMusicPlayer/UWPMusicPlayer/Models/ItemsBuilder.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UWPMusicPlayer.Pages;
 
 namespace UWPMusicPlayer.Models
 {
     /// <summary>
-    /// create the want menus
+    /// create the want menus and setting menu buttom function
+    /// 创建你所需要的菜单栏内容，添加菜单栏按钮功能。
     /// </summary>
     public static class ItemsBuilder
     {
@@ -16,30 +18,37 @@ namespace UWPMusicPlayer.Models
         private static IList<NavItem> _footItems = null;
         private static IList<NavItem> _navItems = null;
 
+        /// <summary>
+        /// Build Menu Item Navigation : PlayList, Recently, Playing. 建立菜单项目导航
+        /// </summary>
+        /// <returns></returns>
         public static IList<NavItem> BuildNavItems()
         {
             if (_navItems == null)
             {
                 _navItems = new List<NavItem>()
                 {
-                    new NavItem() {Icon = "\xE8D6", Title="My Music", View=null}, //View=typeof(MyMusicPage)
-                    new NavItem() { Icon = "\xE121", Title="Recently", View=null},
-                    new NavItem() { Icon = "\xE142", Title="Playing", View=null},
+                    new NavItem() {Icon = "\xE8D6", Title="PlayList", View=typeof(MyMusicPage)}, //View=typeof(MyMusicPage).
+                    new NavItem() { Icon = "\xE121", Title="Recently", View=typeof(RecentlyPage)}, // 把null换成typeof(你想要跳转的页面，like RentlyPage).
+                    new NavItem() { Icon = "\xE142", Title="Playing", View=typeof(PlayingPage)}, // View=typeof(PlayingPage)
                 };
             }
             return _navItems;
         }
 
 
-
+        /// <summary>
+        /// Build Menu Foot Item Navigation: Setting, Feedback.   建立菜单底部项目导航
+        /// </summary>
+        /// <returns></returns>
         public static IList<NavItem> BuildFootItems()
         {
             if (_footItems == null)
             {
                 _footItems = new List<NavItem>()
-                {
-                    new NavItem() { Icon = "\xE76E", Title="Feedback", View = null},
-                    new NavItem() { Icon = "\xE713", Title="Setting", View = null},
+                {                                        // The two page setting is doesn't work????
+                    new NavItem() { Icon = "\xE713", Title="Setting", View = typeof(SettingPage) }, //View=typeof(SettingPage)
+                    new NavItem() { Icon = "\xE76E", Title="Feedback", View = typeof(FeedbackPage) }, // View=typeof(FeedbackPage)
                 };
             }
             return _footItems;
