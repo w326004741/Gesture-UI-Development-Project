a77390980310b745e23ed074e8f2fe64030fd00e
w326004741 <326004741@qq.com>
2018-03-20 17:30:20 +0000

    update MusicConverter

---
+++ UWPMusicPlayer/UWPMusicPlayer/Converter/MusicConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace UWPMusicPlayer.Converter
+{
+    public class MusicConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return ((TimeSpan)value).TotalSeconds;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return TimeSpan.FromSeconds((double)value);
+        }
+    }
+
+}
